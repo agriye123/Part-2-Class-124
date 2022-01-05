b3ffe86efe785c386f8b4a875413abdbe2840d34
agriye123 <88137750+agriye123@users.noreply.github.com>
2022-01-05 15:27:20 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,27 @@
+function setup()
+{
+    video=createCapture(VIDEO);
+    video.size(550,500);
+
+    canvas=createCanvas(550,500);
+    canvas.position(560,160);
+
+    poseNet=ml5.poseNet(video,modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function draw()
+{
+    background('#D22B2B')
+}
+function modelLoaded()
+{
+    console.log("Posenet is initialised");
+}
+function gotPoses(results)
+{
+if(results.length>0)
+{
+    console.log(results);
+}
+}
